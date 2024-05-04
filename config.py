d5bfc008129297bf42c89c673d9a89c3d4af2a88
Gorbash1370 <144901675+gorbash1370@users.noreply.github.com>
2024-05-04 17:00:58 +0100

    Added custom exception MismatchError and reworded docstring of EmptyDictionaryError in config.py.

--- config.py
+++ config.py
@@ -1,6 +1,22 @@
 bing_api_key = "your_key_here"
 
-############## CUSTOM EXCEPTION ##############
+############## CUSTOM EXCEPTIONS ##############
 class EmptyDictionaryError(Exception):
-    """Raised when the books_data dictionary is empty. This suggests the web scraping has totally failed, which is likely due to an incorrect URL or an incompatible Humble Bunde webpage structure relative to this script."""
+    """
+    Exception raised when the books_data dictionary is empty.
+    
+    Typically indicates that web scraping failed, possibly due to an incorrect URL or an incompatible Humble Bundle webpage structure.
+    
+    Check the URL and the expected HTML structure relative to the HTML fields parsed for Publisher, Title, Author(s), Blurb.
+    """
+    pass
+
+class MismatchError(Exception):
+    """
+    Exception raised when the counts of titles, authors, and blurbs parsed from the Humble Bundle HTML content do not match.
+    
+    This mismatch prevents the creation of a coherent books_data dictionar. Script termination is enacted to avoid erroneous API calls or misleading txt file content
+    
+    Ensure the webpage structure is compatible with the HTML parsing logic.
+    """
     pass
